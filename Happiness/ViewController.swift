4e4f6c0a6fc9e0951d494a1c302c1d4b73fc61be
Isabel Diaz Miranda <isabeldiazmi@gmail.com>
2016-01-02 15:54:27 -0600

    Initial Commit

---
+++ Happiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happiness
+//
+//  Created by Isabel Díaz Miranda on 02/01/16.
+//  Copyright © 2016 Isabel Díaz Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
