3b49eed2455d01b53529f5b92824b9adce1297b9
Isabel Diaz Miranda <isabeldiazmi@gmail.com>
2016-01-16 18:05:38 -0600

    Views in Swift

---
+++ HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Isabel Díaz Miranda on 02/01/16.
+//  Copyright © 2016 Isabel Díaz Miranda. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+    
+}
